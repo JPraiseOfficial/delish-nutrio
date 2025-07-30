6de4a4daa72f6d048893f25d2dd7908666c6421f
JPraiseOfficial <jimmypraiseofficial@gmail.com>
2025-07-30 15:46:31 +0100

    added code to exit process on databse connection failure

--- src/config/mongodb.ts
+++ src/config/mongodb.ts
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 import { env } from "./env.js";
 
-export const connectMongoDB = async () => { 
-    try {
-        await mongoose.connect(env.mongodb.uri); 
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.error("Error connecting to MongoDB", error);
-    }
-}
+export const connectMongoDB = async () => {
+  try {
+    await mongoose.connect(env.mongodb.uri);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB", error);
+    process.exit(1);
+  }
+};
--- src/config/mysqldb.ts
+++ src/config/mysqldb.ts
@@ -1,24 +1,25 @@
 import { Sequelize } from "sequelize";
 import { env } from "./env.js";
 
-let databaseName = env.mysqldb.name
-let username = env.mysqldb.user
-let password = env.mysqldb.password
+let databaseName = env.mysqldb.name;
+let username = env.mysqldb.user;
+let password = env.mysqldb.password;
 
 const mysqldb = new Sequelize(databaseName, username, password, {
   host: env.mysqldb.host,
   port: env.mysqldb.port,
-  dialect: 'mysql'
+  dialect: "mysql",
 });
 
 export const connectMySQL = async () => {
   try {
     await mysqldb.authenticate();
-    console.log('Connection has been established successfully.');
+    console.log("Connection has been established successfully.");
     await mysqldb.sync();
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error("Unable to connect to the database:", error);
+    process.exit(1);
   }
-}
+};
 
-export default mysqldb
+export default mysqldb;
